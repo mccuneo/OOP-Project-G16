1399fc332cd82f2c7a40cd0d58bf9755295c4725
mccuneo <128420816+mccuneo@users.noreply.github.com>
2023-03-22 15:26:56 -0400

    Room.h

--- ConsoleApplication1/Room.h
+++ ConsoleApplication1/Room.h
@@ -1,6 +1,14 @@
+#include <string>
+#include iostream
+using namespace;
 class Room : public Person
 
 class Room {
-
-    
+ private:
+double m_roomnum;
+string m_roomtype;
+string m_guestname;
+public:
+Room ();
+Room (double n, string t, string g)
 }
