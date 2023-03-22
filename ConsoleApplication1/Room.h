e4bed0a43a4cb2923e1357d56823cd527bd11ee9
mccuneo <128420816+mccuneo@users.noreply.github.com>
2023-03-22 15:51:21 -0400

    .

--- ConsoleApplication1/Room.h
+++ ConsoleApplication1/Room.h
@@ -1,4 +1,18 @@
-class Room : public Person
+class Room : public Person;
 class Room {
-    
+    private:
+    int m_roomnum;
+    string m_roomtype;
+    string m_guestname;
+    public:
+    Room();
+    Room (int n, string t, string g);
+    //Getters and Setters
+    int Getroomnum(){return m_roomnum;};
+    string Getroomnum(){return m_roomtype;};
+    string Getroomnum(){return m_guestname;};
+     void Setroomnum(int n){m_roomnum = n;};
+     void Setroomtype(string t){m_roomtype = t;};
+     void Setguestname(string g){m_guestname = g;};
+
 }
